d8d6f8e6bccf26b9010374889539db57bff0d126
Kaveh Yousefi <KavehYousefi1401@gmail.com>
2023-05-17 14:40:31 +0200

    Supplementation of Unicode support note
    Supplemented a note concerning Unicode support in Common Lisp.

--- 🕳️/🕳️_001/main.lisp
+++ 🕳️/🕳️_001/main.lisp
@@ -156,10 +156,18 @@
 ;; lifetime, but restricted in their visibility to select occasions that
 ;; require express injuction.
 ;; 
-;; Please remember that special variables, ligated into a consanguinity
-;; with global variables as a general species, and exacerbated by their
-;; implicit and contingently arbitrary declarations, merit the wite of
-;; encumbering programs with superfluous complexity.
+;; It constitutes a peisant element of gnarity to remember that special
+;; variables, ligated into a consanguinity with global variables as a
+;; general species, and exacerbated by their implicit and contingently
+;; arbitrary declarations, merit the wite of encumbering programs with
+;; superfluous complexity.
+;; 
+;; Please note that the concrete character set deployed constitutes a
+;; dependency on the Common Lisp implementation; in corollary, Unicode
+;; support may or may not be a feature incorporated in the personal
+;; environment. The interpreter at hand has been developed and tested
+;; with "Steel Bank Common Lisp" (SBCL) version 1.1.4 as part of the
+;; "Lisp Cabinet 0.3.5" bundle ([christensen2013lispcabinet035]).
 ;; 
 ;; --------------------------------------------------------------------
 ;; 
