79da181d157921d9a1f0b00d4247610eb7ec17ad
Kaveh Yousefi <KavehYousefi1401@gmail.com>
2023-08-21 11:59:07 +0200

    Extension of documentation
    Extended the documentation.

--- Typespam/Typespam_001/main.lisp
+++ Typespam/Typespam_001/main.lisp
@@ -1,7 +1,132 @@
-;; Date: 2022-01-04
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 
+;; This program implements an interpreter for the esoteric programming
+;; language "Typespam", invented by the Esolang user "TheZipCreator" and
+;; presented on August 4th, 2017, the concept of which loosely derives
+;; from Urban Mueller's language "brainfuck', distinguished, however,
+;; in the visualization of its memory as a vertically aligned "chain" in
+;; lieu of a horizontal tape, the same admits integers as well as
+;; characters as "instances", and the kenspeckle control flow mechanims
+;; based upon markers.
+;; 
+;; 
+;; Concept
+;; =======
+;; Typespam programs operate on a vertical arrangement of cells, the
+;; "chain", each component of which may store an "instance", that is,
+;; either a character or an integer.
+;; 
+;; == CONTROL FLOW IS MARKER-BASED ==
+;; A dioristic element of its haecceity, Typespam permits the duction of
+;; the program flow via marker-based gotos.
+;; 
+;; The command "|" interprets the subsequent character as a new marker
+;; name, the same is registered with the instruction pointer (IP)
+;; location. Transpiring from an encounter with the instruction "}",
+;; itself expecting a succeeding character as such an identifier, the
+;; marker answering to this character is retrieved, and the instruction
+;; pointe relocated to the memorized position.
+;; 
+;; 
+;; Architecture
+;; ============
+;; An aspect of its indicium, Typespam supplants the infinite tape of
+;; brainfuck, usually conceived as a horizontal extent of byte-valued
+;; cells, by the vertically aligned innumerate chain, an entity of
+;; enhanced potence, as it embraces any integer or character.
+;; 
+;; == THE CHAIN: A VERTICAL SERIES OF CELLS ==
+;; The chain's ordonnance registers an infinite account of cells,
+;; designed in a vertical arrangement.
+;; 
+;; == THE CELL: A CHARACTER OR INTEGER CONTAINER ==
+;; The proprium of each of chain's components is limned by its ability
+;; to amplect an aefauld instance, that is, a character or integer of
+;; any sign and magnitude. A warklume of this diversity, associated with
+;; any cell is its type.
+;; 
+;; It bears the similitude of a variable that a cell is not responsive
+;; to actions in its inchoate state; prior to its employment, an
+;; initialization ought to be applied to homologate examinations or
+;; modifications.
+;; 
+;; == THE CELL POINTER: A CURSOR ==
+;; At any instance, a cursor, known as the "cell pointer", selects the
+;; currently active unit among the cells, its amenability extending to
+;; all indagations and manipulations. Proeeding from the chain's
+;; idopathic design, the pointer is illustrated as a mobile object that
+;; may be translated upwards or downwards.
+;; 
+;; 
+;; Data Types
+;; ==========
+;; Typespam's bivial type system distinguishes its entities, known as
+;; instances, into characters and signed integers of any magnitude.
+;; 
+;; 
+;; Instructions
+;; ============
+;; Typespam tallies a decimal of instructions, their wikes covering the
+;; bailiwicks of chain management in topology and content, input,
+;; output, as well as a marker-based control flow helming.
+;; 
+;; == OVERVIEW ==
+;; A cursory apercu shall communicate the foundational aspects of the
+;; language's competences:
+;; 
+;;   ------------------------------------------------------------------
+;;   Command | Effect
+;;   --------+---------------------------------------------------------
+;;   _       | Initializes the current cell.
+;;   ..................................................................
+;;   ^       | Moves the cell pointer one step up the chain.
+;;   ..................................................................
+;;   v       | Moves the cell pointer one step down the chain.
+;;   ..................................................................
+;;   c       | Designates the current cell as capable of holding a
+;;           | character instance.
+;;   ..................................................................
+;;   i       | Designates the current cell as capable of holding an
+;;           | integer instance.
+;;   ..................................................................
+;;   {       | Consumes the subsequent character or integer following
+;;           | this "{" command and stores it in the current cell.
+;;   ..................................................................
+;;   |       | Consumes the next character in the program and creates a
+;;           | new marker which associates this character as a label
+;;           | with the position of its encounter for future jump
+;;           | commands, for which please see the "}" operation.
+;;           |---------------------------------------------------------
+;;           | Any already extant marker with the same name is tacitly
+;;           | superceded by a new definition.
+;;   ..................................................................
+;;   }       | Consumes the next character as a marker name. If the
+;;           | current cell's value is not zero (0), moves the
+;;           | instruction pointer (IP) to the position associated with
+;;           | this marker. Otherwise proceeds as usual.
+;;           |---------------------------------------------------------
+;;           | An error of an unspecified type is signaled if the
+;;           | marker name does not correspond to any registered
+;;           | entity.
+;;   ..................................................................
+;;   -       | Displays the value of the current cell in the standard
+;;           | output.
+;;   ..................................................................
+;;   #       | Queries the standard input for a character and stores
+;;           | it in the current cell.
+;;   ------------------------------------------------------------------
+;; 
+;; --------------------------------------------------------------------
+;; 
+;; Author: Kaveh Yousefi
+;; Date:   2022-01-04
 ;; 
 ;; Sources:
-;;   -> "https://esolangs.org/wiki/Typespam"
+;;   [esolang2020Typespam]
+;;   The Esolang contributors, "Typespam", June 15th, 2020
+;;   URL: "https://esolangs.org/wiki/Typespam"
+;; 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
 
@@ -42,10 +167,10 @@
 ;;; -------------------------------------------------------
 
 (deftype instance-type ()
-  "The ``instance-type'' defines the possibly data types that an
+  "The ``instance-type'' defines the possible data types that an
    instance is permitted to assume.
    ---
-   Apart from the canonical elements, a special sentinel ``:unknown''
+   Apart from the canonical elements, a special sentinel, ``:unknown'',
    accommodates a designator for the undecided initial instance state."
   '(member :unknown :character :integer))
 
@@ -69,7 +194,7 @@
 ;;; -------------------------------------------------------
 
 (defun cell-display (cell &optional (destination T))
-  "Prints the CELL's value to the DESTINATION, with defaults to ``T'',
+  "Prints the CELL's value to the DESTINATION, which defaults to ``T'',
    and returns the CELL."
   (declare (type Cell        cell))
   (declare (type destination destination))
@@ -77,7 +202,7 @@
     (:character
       (write-char (code-char (cell-value cell)) destination))
     (:integer
-      (format destination "~d" (cell-value cell)))
+      (format destination " ~d" (cell-value cell)))
     (otherwise
       (error "Cell cannot display as a type of ~s." (cell-type cell))))
   (the Cell cell))
@@ -85,8 +210,9 @@
 ;;; -------------------------------------------------------
 
 (defun cell-is-nonzero (cell)
-  "Checks whether the CELL containas a non-zero value, returning a
-   ``boolean'' result of ``T'' upon confirmation, and ``NIL'' otherwise."
+  "Checks whether the CELL contains a non-zero value, returning a
+   ``boolean'' result of ``T'' upon confirmation, and ``NIL''
+   otherwise."
   (declare (type Cell cell))
   (the boolean (not (zerop (cell-value cell)))))
 
@@ -143,8 +269,9 @@
             (the Cell (gethash pointer chain)))
            
            (check-if-initialized ()
-            "Checks whether the current instance is selected, throwing an
-             error if not, otherwise returning no value."
+            "Checks whether the current instance is selected, throwing
+             an error of an unspecified type if not, otherwise simply
+             returning no value."
             (unless (gethash pointer chain)
               (error "The instance at the index ~d is not initialized."
                 pointer))
@@ -154,21 +281,21 @@
            (set-marker (marker-name marker-position)
             "Associates the MARKER-NAME with the MARKER-POSITION in the
              CODE, overwriting any extant entry with such a name, and
-             returning no value."
+             returns no value."
             (declare (type character marker-name))
             (declare (type fixnum    marker-position))
             (setf (gethash marker-name markers) marker-position)
             (values))
            
            (get-marker-position (marker-name)
-            "Returns the position in the CODE designating the start of the
-             body of the marker with the MARKER-NAME.
+            "Returns the position in the CODE designating the start of
+             the body of the marker with the MARKER-NAME.
              ---
              If no MARKER-NAME association exists yet, the respective
              declaration is searched starting from the beginning of the
              CODE, and, if found, an association is established ere its
-             returning. If no marker with this identifier can be detected,
-             an error is signaled."
+             returning. If no marker with this identifier can be
+             detected, an error is signaled."
             (declare (type character marker-name))
             (multiple-value-bind (marker-position contains-marker)
                 (gethash marker-name markers)
