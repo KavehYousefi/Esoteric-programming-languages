7f8e67c9d54d2097cec1acf890614560547fbb63
Kaveh Yousefi <KavehYousefi1401@gmail.com>
2023-09-11 02:15:54 +0200

    Supplementation of Unicode support note
    Supplemented a note regarding the implementation-dependent state of Unicode support in Common Lisp.

--- Headsecks/Headsecks 001/main.lisp
+++ Headsecks/Headsecks 001/main.lisp
@@ -91,12 +91,33 @@
 ;;   7                | ]
 ;;   ------------------------------------------
 ;; 
+;; 
+;; Implementation
+;; ==============
+;; This project has been implemented in Common Lisp, its circumference
+;; amplecting a Headsecks code interpreter, as well as bidirectional
+;; conversion routines betwixt the language and its entheus brainfuck.
+;; 
+;; == UNICODE SUPPORT IS IMPLEMENTATION-DEPENDENT ==
+;; Please note that the concrete character set deployed constitutes a
+;; dependency on the Common Lisp implementation; in corollary, Unicode
+;; support may or may not be a feature incorporated in the personal
+;; environment. The interpreter at hand has been developed and tested
+;; with "Steel Bank Common Lisp" (SBCL) version 1.1.4 as part of the
+;; "Lisp Cabinet 0.3.5" bundle ([christensen2013lispcabinet035]).
+;; 
 ;; --------------------------------------------------------------------
 ;; 
 ;; Author: Kaveh Yousefi
 ;; Date:   2023-09-10
 ;; 
 ;; Sources:
+;;   [christensen2013lispcabinet035]
+;;   G. Christensen, "Lisp Cabinet 0.3.5", 2013
+;;   URL: "https://sourceforge.net/projects/lispcabinet/"
+;;   Notes:
+;;     - Download page of the "Lisp Cabinet" project.
+;;   
 ;;   [esolang2016Headsecks]
 ;;   The Esolang contributors, "Headsecks", March 16, 2016
 ;;   URL: "https://esolangs.org/wiki/Headsecks"
