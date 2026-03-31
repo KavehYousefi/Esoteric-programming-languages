ad2ab8dacc68667c965d9693259868c86f084900
Kaveh Yousefi <KavehYousefi1401@gmail.com>
2026-03-31 22:30:38 +0200

    Improvement of the EBNF description
    Supplemented to the Extended Backus-Naur Form (EBNF) description's integer literal specification, “integerLiteral”, the optional sign.

--- Adj/Adj 001/main.lisp
+++ Adj/Adj 001/main.lisp
@@ -149,7 +149,7 @@
 ;;   
 ;;   operand              := integerLiteral | variable ;
 ;;   variable             := "a" | "b" | "c" ;
-;;   integerLiteral       := digit , { digit } ;
+;;   integerLiteral       := [ "+" | "-" ] , digit , { digit } ;
 ;;   digit                := "0" | "1" | "2" | "3" | "4"
 ;;                        |  "5" | "6" | "7" | "8" | "9"
 ;;                        ;
